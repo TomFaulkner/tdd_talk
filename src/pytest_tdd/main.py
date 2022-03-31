197c02ac8f8ed6e2d5dbbea3cd31368c48f69223
Tom Faulkner <tomfaulkner@gmail.com>
2022-03-31 16:33:32 -0500

    solutions

--- src/pytest_tdd/main.py
+++ src/pytest_tdd/main.py
@@ -2,12 +2,18 @@ def return_same(param):
     return param
 
 
-def append_to_list(param, items=[]):
-    return items.append(param)
+def append_to_list(param, items=None):
+    if not items:
+        items = []
+    items.append(param)
+    return items
 
 
 def is_palindrome(phrase):
-    pass
+    return (
+        phrase.lower().replace('?', '').replace(' ', '')
+        == ''.join(reversed(phrase.lower().replace('?', '').replace(' ', '')))
+    )
 
 
 def complex_(a, b, c):
